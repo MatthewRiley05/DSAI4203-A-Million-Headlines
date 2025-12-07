1d40a450ce93913019b57c3e27266f331b4dbc17
Yerasyk <urynbasarov56@gmail.com>
2025-12-07 18:43:12 +0800

    Removing duplicates

--- scripts/1_data_cleaning.py
+++ scripts/1_data_cleaning.py
@@ -6,5 +6,6 @@ os.makedirs("outputs", exist_ok=True)
 
 df = pd.read_csv("abcnews-date-text.csv")
 df[df.columns[0]] = pd.to_datetime(df[df.columns[0]], format="%Y%m%d", errors="coerce")
+df = df.drop_duplicates('headline_text')
 df.to_csv("outputs/abcnews-date-text-preprocessed.csv", index=False)
 print("Data cleaning complete!")
